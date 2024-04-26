41d48085c123fe0c9ec5dad37cdebeb0e3629a0b
kjw2948 <kjw970313@cau.ac.kr>
2024-04-26 16:16:32 +0900

    init

---
+++ src/main/java/kim/bidetchoong/BidetchoongApplication.java
@@ -0,0 +1,13 @@
+package kim.bidetchoong;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BidetchoongApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BidetchoongApplication.class, args);
+	}
+
+}
---
+++ src/test/java/kim/bidetchoong/BidetchoongApplicationTests.java
@@ -0,0 +1,13 @@
+package kim.bidetchoong;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BidetchoongApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
