64bf857d76dc147fb2c3ff1a58a38f9fde8e0140
kjw2948 <kjw970313@cau.ac.kr>
2024-06-22 16:04:08 +0900

    [update] Repository 저장 , 조회 로직 추가

--- src/main/java/kim/bidetchoong/domain/repository/MemberRepository.java
+++ src/main/java/kim/bidetchoong/domain/repository/MemberRepository.java
@@ -3,19 +3,25 @@ package kim.bidetchoong.domain.repository;
 import kim.bidetchoong.domain.member.Member;
 import org.springframework.stereotype.Repository;
 
+import java.util.ArrayList;
 import java.util.HashMap;
+import java.util.List;
 import java.util.Map;
 
 @Repository
 public class MemberRepository {
-    private static Map<Long, Member> store = new HashMap<>();
+    private static Map<Long, Member> memberStore = new HashMap<>();
 
     public Long save(Member member) {
-        store.put(member.getId(), member);
+        memberStore.put(member.getId(), member);
         return member.getId();
     }
 
     public Member findById(Long id) {
-        return store.get(id);
+        return memberStore.get(id);
+    }
+
+    public List<Member> findAll() {
+        return new ArrayList<>(memberStore.values());
     }
 }
--- src/main/java/kim/bidetchoong/domain/repository/ToiletRepository.java
+++ src/main/java/kim/bidetchoong/domain/repository/ToiletRepository.java
@@ -3,16 +3,27 @@ package kim.bidetchoong.domain.repository;
 import kim.bidetchoong.domain.toilet.Toilet;
 import org.springframework.stereotype.Repository;
 
+import java.util.ArrayList;
 import java.util.HashMap;
+import java.util.List;
 import java.util.Map;
 
 @Repository
 public class ToiletRepository {
 
-    private static Map<Long, Toilet> store = new HashMap<>();
+    private static Map<Long, Toilet> toiletStore = new HashMap<>();
     public Long save(Toilet toilet) {
-        store.put(toilet.getId(), toilet);
+        toiletStore.put(toilet.getId(), toilet);
         return toilet.getId();
     }
+
+    public Toilet findById(Long id) {
+        return toiletStore.get(id);
+    }
+
+    public List<Toilet> findAll() {
+        return new ArrayList<>(toiletStore.values());
+    }
+
 
 }
